d571b41f1297807e1bfa249f916e903c5c102793
amogharajsandur <s865music@gmail.com>
2025-07-02 16:41:56 +0530

    Added info button and updated styling; Enhanced color change functionality;

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 let backgroundColor = document.querySelector("body");
+let mainContent = document.querySelector("main");
 let button = document.getElementById("change-colour");
-let colorText = document.getElementById("color-text");
+let colorText = document.querySelectorAll(".color-text");
+let infoButton = document.getElementById("info");
+let infoIcon = document.getElementById("info-icon");
+let infoBox = document.querySelector(".info-box");
 
 // colors is the array containing all the colors
 let colors = ["red", "yellow", "green", "blue", "black", "white", "pink", "purple", "orange", "grey"];
 let value = Math.floor(Math.random()*10);
+let infoVisible = false;
 
 // alert("Change the color of the HTML doc with just a mouse click!")
 
 button.addEventListener("click", changeColor);
+infoButton.addEventListener("click", showInfo);
 
 // This function generates a random number between 0-9
 function randomNumber() {
@@ -18,8 +24,24 @@ function randomNumber() {
 
 // This function updates the background color
 function changeColor() {
-    let i = randomNumber()
+    let i = randomNumber();
     backgroundColor.style.backgroundColor = colors[i];
-    colorText.style.color = colors[i]
-    console.log(colors[i])
+    colorText.forEach(element => 
+        {element.style.color = colors[i];}
+    );
+    // colorText.style.color = colors[i];
+}
+
+function showInfo() {
+    if (infoVisible === false) {
+        // infoBox.style.display = "flex";
+        infoIcon.style.cssText = "rotate: 180deg; transition: ease-in-out 0.1s;";
+        infoBox.style.cssText = "display: flex; transition: ease-in-out 0.3s;";
+        infoVisible = true;
+    } else if (infoVisible) {
+        // infoBox.style.display = "none";
+        infoIcon.style.cssText = "rotate: 0; transition: ease-in-out 0.1s;";
+        infoBox.style.cssText = "display: none; transition: ease-in-out 0.3s;"
+        infoVisible = false;
+    }
 }
