b73b8384282828c9d802dbdf5c1108ca99336724
amogharajsandur <s865music@gmail.com>
2025-07-02 07:52:05 +0530

    Add button to change background color and update styles

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 let backgroundColor = document.querySelector("body");
 let button = document.getElementById("change-colour");
 // colors is the array containing all the colors
-let colors = ["red", "yellow", "green", "blue", "black", "white"];
+let colors = ["red", "yellow", "green", "blue", "black", "white", "pink", "purple", "orange", "grey"];
+let colorText = document.getElementById("color-text");
+let value = Math.floor(Math.random()*10);
 
-changeColor.addEventListener("click", changeColor());
+button.addEventListener("click", changeColor);
+
+function randomNumber() {
+    value = Math.floor(Math.random()*10);
+    return value;
+}
 
 function changeColor() {
-    backgroundColor.innerHTML = `
-        <button id="change-colour">Change Color</button>
-    `
-    console.log(backgroundColor)
+    backgroundColor.style.backgroundColor = colors[randomNumber()];
+    // backgroundColor.innerHTML = `
+    // <button type="button" id="change-colour">Change Color</button>
+    // `
+    console.log(randomNumber());
     // for (let i=0; i<colors.length, i++;) {
     // }
 }
